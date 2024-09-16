3c52c705d2f4a4a4113aaf6ab4ca451b798c8c69
Tetiana Mandziuk <tetianaman@wix.com>
2024-09-16 20:01:37 +0200

    pr comments

--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -1,14 +1,24 @@
-import { HeroImage } from '~/components/hero-image/hero-image';
+import styles from './index.module.scss';
+import { CategoryLink } from '~/components/category-link/category-link';
+import { LabelWithArrow } from '~/components/label-with-arrow/label-with-arrow';
 
 export default function HomePage() {
     return (
         <>
-            <HeroImage
-                subTitle="ReClaim"
-                title="Reuse. Repurpose. Relove."
-                linkLabel="Shop Collections"
-                linkCategorySlug="all-products"
-            />
+            <div className={styles.heroBanner}>
+                <img
+                    src="src/assets/img/heroImage.jpg"
+                    className={styles.image}
+                    alt="Hero banner"
+                />
+                <div className={styles.overlay}>
+                    <div className={styles.subtitle}>ReClaim</div>
+                    <h1 className={styles.title}>Reuse. Repurpose. Relove.</h1>
+                    <CategoryLink categorySlug="all-products" className={styles.overlayLink}>
+                        <LabelWithArrow>Shop Collections</LabelWithArrow>
+                    </CategoryLink>
+                </div>
+            </div>
             <p>Home page</p>
         </>
     );
