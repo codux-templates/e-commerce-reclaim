da12bd450171bcdeafb497c7d3c899624051d70c
Tetiana Mandziuk <tetianaman@wix.com>
2024-09-16 11:02:01 +0200

    board

--- src/_codux/boards/new-board.board.tsx
+++ src/_codux/boards/new-board.board.tsx
@@ -1,10 +1,11 @@
+import './new-board.board.css';
 import { createBoard } from '@wixc3/react-board';
 
 export default createBoard({
     name: 'New Board',
     Board: () => (
         <div>
-            <h1>Heading 1</h1>
+            <h1 className="heading1">Heading 1</h1>
         </div>
     ),
     isSnippet: true,
