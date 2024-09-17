228e01bac28efec6fae3654e8d072a8e6bb16fb3
Tetiana Mandziuk <tetianaman@wix.com>
2024-09-17 18:11:38 +0200

    pr comments

--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -1,13 +1,12 @@
 import { CategoryLink } from '~/components/category-link/category-link';
 import styles from './index.module.scss';
-import heroImageJpg from '~/assets/img/heroImage.jpg';
 import { LabelWithArrow } from '~/components/label-with-arrow/label-with-arrow';
 
 export default function HomePage() {
     return (
         <>
-            <div className={styles.heroImage}>
-                <img src={heroImageJpg} alt="Hero banner" />
+            <div className={styles.heroBanner}>
+                <img src="https://static.wixstatic.com/media/32aab9_2c3c65e142434906992aedb17db53566~mv2.jpg" />
                 <div className={styles.overlay}>
                     <div className={styles.subtitle}>ReClaim</div>
                     <h1 className={styles.title}>Reuse. Repurpose. Relove.</h1>
