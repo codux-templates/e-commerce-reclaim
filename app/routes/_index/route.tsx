57ed8537db9efc5d88c2d88850ac70ed61366ad4
Tetiana Mandziuk <tetianaman@wix.com>
2024-09-18 16:01:43 +0200

    update

--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -29,30 +29,30 @@ export default function HomePage() {
                 </CategoryLink>
             </div>
 
-            <div className={styles.rowOfCards}>
-                <CategoryLink categorySlug="kitchen" className="cardZoom">
+            <div className="rowOfCards">
+                <CategoryLink categorySlug="kitchen" className="linkCard">
                     <img
-                        className="cardBackground"
+                        className="linkCardBackground"
                         src="https://static.wixstatic.com/media/c837a6_c05a03f48fbd49e7b5046d1b18c930eb~mv2.jpg/v1/fill/w_547,h_730,q_90/c837a6_c05a03f48fbd49e7b5046d1b18c930eb~mv2.jpg"
                         alt=""
                     />
-                    <div className="cardTitle">Kitchen</div>
+                    <div className="linkCardTitle">Kitchen</div>
                 </CategoryLink>
-                <CategoryLink categorySlug="bath" className="cardZoom">
+                <CategoryLink categorySlug="bath" className="linkCard">
                     <img
-                        className="cardBackground"
+                        className="linkCardBackground"
                         src="https://static.wixstatic.com/media/c837a6_269f35d6ccff4321b7ed1e65c2835c61~mv2.jpg/v1/fill/w_548,h_730,q_90/c837a6_269f35d6ccff4321b7ed1e65c2835c61~mv2.jpg"
                         alt=""
                     />
-                    <div className="cardTitle">Bath</div>
+                    <div className="linkCardTitle">Bath</div>
                 </CategoryLink>
-                <CategoryLink categorySlug="on-the-go" className="cardZoom">
+                <CategoryLink categorySlug="on-the-go" className="linkCard">
                     <img
-                        className="cardBackground"
+                        className="linkCardBackground"
                         src="https://static.wixstatic.com/media/c837a6_d38d8d08196d477ba49efff880d5b918~mv2.jpg/v1/fill/w_547,h_730,q_90/c837a6_d38d8d08196d477ba49efff880d5b918~mv2.jpg"
                         alt=""
                     />
-                    <div className="cardTitle">On the Go</div>
+                    <div className="linkCardTitle">On the Go</div>
                 </CategoryLink>
             </div>
         </>
