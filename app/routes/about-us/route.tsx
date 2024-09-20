c9a8267047a3c77c8e9661802b99b4dd62b9ddb8
Yurii Nesteruk <yuriin@wix.com>
2024-09-20 16:57:02 +0300

    after review

--- app/routes/about-us/route.tsx
+++ app/routes/about-us/route.tsx
@@ -4,13 +4,13 @@ export default function AboutUsPage() {
     return (
         <div className={styles.root}>
             <div className={styles.aboutSection}>
-                <h1 className={styles.header1}>We are ReClaim</h1>
-                <p className={styles.header2}>A women-owned local business</p>
-                <p className={styles.description}>
+                <h1 className={styles.title}>We are ReClaim</h1>
+                <div className={styles.subtitle}>A women-owned local business</div>
+                <div className={styles.description}>
                     This is the space to introduce visitors to the business or brand. Briefly
                     explain who&#39;s behind it, what it does and what makes it unique. Share its
                     core values and what this site has to offer.
-                </p>
+                </div>
             </div>
             <img
                 className={styles.image}
