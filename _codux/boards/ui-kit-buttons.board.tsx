d566f4ba4a0b45898d0341e05b1760d8734302ae
Arnon Kehat <arnon@liveperson.com>
2024-09-22 13:54:50 +0300

    added necessary class selectors to board

--- _codux/boards/ui-kit-buttons.board.tsx
+++ _codux/boards/ui-kit-buttons.board.tsx
@@ -1,18 +1,20 @@
-import { createBoard } from '@wixc3/react-board';
-import { Button } from '../../src/components/button/button';
+import { createBoard, Variant } from '@wixc3/react-board';
+import '~/styles/common.scss';
+import classNames from 'classnames';
 import styles from './ui-kit-buttons.board.module.scss';
+import { Button } from '../../src/components/button/button';
 import { LabelWithArrow } from '../../src/components/label-with-arrow/label-with-arrow';
-import { Link, NavLink } from '@remix-run/react';
-import { ROUTES } from '~/router/config';
 
 export default createBoard({
     name: 'UI-Kit Buttons',
     Board: () => (
-        <div className={styles.container}>
+        <div>
             <div>
                 <h2>Buttons</h2>
                 <div className={styles.primaryContainer}>
-                    <LabelWithArrow>Shop Collection </LabelWithArrow>
+                    <Variant name="Primary Button">
+                        <LabelWithArrow>Shop Collection </LabelWithArrow>
+                    </Variant>
                     <p>Primary </p>
                 </div>
                 <div className={styles.secondaryContainer}>
@@ -20,7 +22,9 @@ export default createBoard({
                     <p>Secondary</p>
                 </div>
                 <div className={styles.addToCartContainer}>
-                    <Button className={styles.fullWidth}>Add to Cart</Button>
+                    <Variant name="Add to Cart Button">
+                        <Button className={styles.fullWidth}>Add to Cart</Button>
+                    </Variant>
                     <p>Add to Cart</p>
                 </div>
             </div>
@@ -31,4 +35,5 @@ export default createBoard({
             </div>
         </div>
     ),
+    isSnippet: true,
 });
