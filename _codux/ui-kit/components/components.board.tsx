302b35351198b60a8560f58a1603bf6534b71ef5
Ran Berkovich <155557865+RanBerkovich1@users.noreply.github.com>
2024-09-23 11:35:31 +0300

    created variables: number input, product card and accordion

--- _codux/ui-kit/components/components.board.tsx
+++ _codux/ui-kit/components/components.board.tsx
@@ -18,9 +18,11 @@ export default createBoard({
                 <h3 className={styles.sectionTitle}>Components &amp; Elements</h3>
                 <h4 className={styles.sectionHeader}>INPUT</h4>
             </div>
-            <QuantityInput value={6} className={styles0.quantityInput1} />
+            <Variant name="Number Input">
+                <QuantityInput value={6} className={styles0.quantityInput1} />
+            </Variant>
             <span className={styles0.fontDetails}>Number Input</span>
-            <Variant name="Heading1">
+            <Variant name="Accordion">
                 <hr className={styles.hrLight} />
                 <h4 className={styles.sectionHeader}>ACCORDION</h4>
                 <Accordion
@@ -44,15 +46,17 @@ export default createBoard({
             <h4>//Labels missing</h4>
             <hr className={styles.hrLight} />
             <h4 className={styles.sectionHeader}>CARDS</h4>
-            <ProductCard
-                name="Bamboo Toothbrush"
-                imageUrl="https://static.wixstatic.com/media/c837a6_18152edaef9940ca88f446ae94b48a47~mv2.jpg/v1/fill/w_824,h_1098,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/c837a6_18152edaef9940ca88f446ae94b48a47~mv2.jpg"
-                ribbon="NEW"
-                priceData={{
-                    currency: '$',
-                    price: 5.5,
-                }}
-            />
+            <Variant name="Product Card">
+                <ProductCard
+                    name="Bamboo Toothbrush"
+                    imageUrl="https://static.wixstatic.com/media/c837a6_18152edaef9940ca88f446ae94b48a47~mv2.jpg/v1/fill/w_824,h_1098,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/c837a6_18152edaef9940ca88f446ae94b48a47~mv2.jpg"
+                    ribbon="NEW"
+                    priceData={{
+                        currency: '$',
+                        price: 5.5,
+                    }}
+                />
+            </Variant>
             <span className={styles0.fontDetails}>Product Card</span>
             <h4>//Product gallery missing</h4>
             <span className={styles0.fontDetails}>Product Card</span>
