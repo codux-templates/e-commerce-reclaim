f5ed408967cadac4c67d50461395533480dc5821
Tetiana Mandziuk <tetianaman@wix.com>
2024-09-26 14:36:01 +0200

    add icons

--- _codux/ui-kit/buttons/buttons.board.tsx
+++ _codux/ui-kit/buttons/buttons.board.tsx
@@ -5,6 +5,12 @@ import { LabelWithArrow } from '~/components/label-with-arrow/label-with-arrow';
 import '../board-common.scss';
 import styles from './buttons.board.module.scss';
 import classNames from 'classnames';
+import { TrashIcon } from '../../../src/components/icons/trash-icon';
+import { LockIcon } from '../../../src/components/icons/lock-icon';
+import { ErrorIcon } from '../../../src/components/icons/error-icon';
+import { MenuIcon } from '../../../src/components/icons/menu-icon';
+import { ChevronRightIcon } from '../../../src/components/icons/chevron-right-icon';
+import { ArrowRightIcon } from '../../../src/components/icons/arrow-right-icon';
 
 export default createBoard({
     name: 'UI Kit Buttons',
@@ -43,9 +49,49 @@ export default createBoard({
 
             <div className="uiKitSection">
                 <div className="uiKitSectionTitle">Icons</div>
-                <div className="uiKitItem">
-                    <CartIcon count={3} />
-                    <div className="uiKitItemDescription">CartIcon</div>
+                <div className={styles.itemsGroup}>
+                    <div className="uiKitItem">
+                        <div className={styles.icon}>
+                            <CartIcon count={3} />
+                        </div>
+                        <div className="uiKitItemDescription">CartIcon</div>
+                    </div>
+                    <div className="uiKitItem">
+                        <div className={styles.icon}>
+                            <TrashIcon className={styles.trashIcon} />
+                        </div>
+                        <div className="uiKitItemDescription">TrashIcon</div>
+                    </div>
+                    <div className="uiKitItem">
+                        <div className={styles.icon}>
+                            <LockIcon className={styles.lockIcon} />
+                        </div>
+                        <div className="uiKitItemDescription">LockIcon</div>
+                    </div>
+                    <div className="uiKitItem">
+                        <div className={styles.icon}>
+                            <ErrorIcon className={styles.errorIcon} />
+                        </div>
+                        <div className="uiKitItemDescription">ErrorIcon</div>
+                    </div>
+                    <div className="uiKitItem">
+                        <div className={styles.icon}>
+                            <MenuIcon className={styles.menuIcon} />
+                        </div>
+                        <div className="uiKitItemDescription">MenuIcon</div>
+                    </div>
+                    <div className="uiKitItem">
+                        <div className={styles.icon}>
+                            <ChevronRightIcon className={styles.chevronRightIcon} />
+                        </div>
+                        <div className="uiKitItemDescription">ChevronIcon</div>
+                    </div>
+                    <div className="uiKitItem">
+                        <div className={styles.icon}>
+                            <ArrowRightIcon className={styles.arrowRightIcon} />
+                        </div>
+                        <div className="uiKitItemDescription">ArrowRightIcon</div>
+                    </div>
                 </div>
             </div>
 
@@ -53,19 +99,13 @@ export default createBoard({
                 <div className="uiKitSectionTitle">Social</div>
                 <div className="uiKitItem">
                     <div className={styles.row}>
-                        <Variant name="WhatsApp">
-                            <WhatsAppIcon className="smallIcon" />
-                        </Variant>
-
-                        <Variant name="Facebook">
-                            <FacebookIcon className="smallIcon" />
-                        </Variant>
-
-                        <Variant name="Pinterest">
-                            <PinterestIcon className="smallIcon" />
-                        </Variant>
+                        <WhatsAppIcon className="smallIcon" />
+                        <FacebookIcon className="smallIcon" />
+                        <PinterestIcon className="smallIcon" />
                     </div>
-                    <div className="uiKitItemDescription">WhatsApp | Facebook | Pinterest</div>
+                    <div className="uiKitItemDescription">
+                        WhatsAppIcon | FacebookIcon | PinterestIcon
+                    </div>
                 </div>
             </div>
         </div>
