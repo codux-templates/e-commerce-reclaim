d2a575898a1a2f4dfd35f16caf8f57fc0b62441a
Codux <codux@codux.com>
2024-09-26 14:22:53 +0300

    fixed discount price when no discount is defined

--- _codux/ui-kit/components/components.board.tsx
+++ _codux/ui-kit/components/components.board.tsx
@@ -1,3 +1,4 @@
+import '../../../src/styles/common.scss';
 import { createBoard, Variant } from '@wixc3/react-board';
 import classNames from 'classnames';
 import { Accordion } from '~/components/accordion/accordion';
@@ -6,6 +7,7 @@ import { QuantityInput } from '~/components/quantity-input/quantity-input';
 import ComponentWrapper from '_codux/board-wrappers/component-wrapper';
 
 import styles from './components.board.module.scss';
+import productCardStyles from '../../../src/components/product-card/product-card.module.scss';
 
 export default createBoard({
     name: 'Components',
@@ -45,17 +47,22 @@ export default createBoard({
                 </Variant>
                 <p className={classNames(styles.variantName, styles.headlinesSpacing)}></p>
                 <h4 className={styles.sectionHeader}>LABELS</h4>
-                <h4>Labels missing</h4>
+                <div className="relativeContainer">
+                    <Variant name="Label">
+                        <span className={productCardStyles.ribbon}>I&apos;M A LABEL</span>
+                    </Variant>
+                </div>
                 <hr className={styles.hrLight} />
                 <h4 className={styles.sectionHeader}>CARDS</h4>
-                <ProductCard
-                    name="Bamboo Toothbrush"
-                    imageUrl="https://static.wixstatic.com/media/c837a6_18152edaef9940ca88f446ae94b48a47~mv2.jpg/v1/fill/w_824,h_1098,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/c837a6_18152edaef9940ca88f446ae94b48a47~mv2.jpg"
-                    ribbon="NEW"
-                    price="$5.5"
-                />
-                <span className={styles.fontDetails}>Product Card</span>
-                <h4>Product gallery missing</h4>
+                <Variant name="Product Card">
+                    <ProductCard
+                        name="Bamboo Toothbrush"
+                        imageUrl="https://static.wixstatic.com/media/c837a6_18152edaef9940ca88f446ae94b48a47~mv2.jpg/v1/fill/w_824,h_1098,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/c837a6_18152edaef9940ca88f446ae94b48a47~mv2.jpg"
+                        ribbon="NEW"
+                        price="$6"
+                        discountedPrice="$5.5"
+                    />
+                </Variant>
                 <span className={styles.fontDetails}>Product Card</span>
             </div>
         </ComponentWrapper>
