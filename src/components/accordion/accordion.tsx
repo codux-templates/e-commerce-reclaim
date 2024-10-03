5cfbf20f70c1c1be888a51723c9fe2773ff8a02b
Oleksii Letushev <oleksiile@wix.com>
2024-10-03 11:34:09 +0200

    update

--- src/components/accordion/accordion.tsx
+++ src/components/accordion/accordion.tsx
@@ -1,5 +1,5 @@
-import { useRef, useState } from 'react';
-import { AnimatePresence, motion } from 'framer-motion';
+import { useState } from 'react';
+import classNames from 'classnames';
 import { getClickableElementAttributes } from '~/utils';
 import { MinusIcon, PlusIcon } from '../icons';
 
@@ -23,64 +23,32 @@ export const Accordion = ({ items, className }: AccordionProps) => {
             {items.map((item, index) => {
                 const isOpen = openItemIndex === index;
                 return (
-                    <AccordionItem
-                        key={index}
-                        isOpen={isOpen}
-                        onToggle={() => setOpenItemIndex(isOpen ? null : index)}
-                        {...item}
-                    />
+                    <div key={index} className={styles.item}>
+                        <div
+                            className={styles.header}
+                            {...getClickableElementAttributes(() =>
+                                setOpenItemIndex(isOpen ? null : index),
+                            )}
+                        >
+                            <p className={styles.title}>{item.title}</p>
+
+                            {isOpen ? (
+                                <MinusIcon className={styles.toggleIcon} />
+                            ) : (
+                                <PlusIcon className={styles.toggleIcon} />
+                            )}
+                        </div>
+
+                        <div
+                            className={classNames(styles.content, {
+                                [styles.expanded]: isOpen,
+                            })}
+                        >
+                            <div className={styles.contentInner}>{item.content}</div>
+                        </div>
+                    </div>
                 );
             })}
-        </div>
-    );
-};
-
-interface AccordionItemProps extends AccordionItem {
-    isOpen: boolean;
-    onToggle: () => void;
-}
-
-const AccordionItem = ({ isOpen, onToggle, title, content }: AccordionItemProps) => {
-    const contentRef = useRef<HTMLDivElement | null>(null);
-
-    const isOpenRef = useRef(isOpen);
-    isOpenRef.current = isOpen;
-
-    const handleAnimationComplete = () => {
-        if (contentRef.current && isOpenRef.current) {
-            // Allow the content to overflow when the accordion item is fully expanded.
-            // This is useful when the content contains an element that, under certain conditions,
-            // must go beyond the content (e.g. a box-shadow on focus).
-            contentRef.current.style.removeProperty('overflow');
-        }
-    };
-
-    return (
-        <div className={styles.item}>
-            <div className={styles.header} {...getClickableElementAttributes(onToggle)}>
-                <p className={styles.title}>{title}</p>
-
-                {isOpen ? (
-                    <MinusIcon className={styles.toggleIcon} />
-                ) : (
-                    <PlusIcon className={styles.toggleIcon} />
-                )}
-            </div>
-
-            <AnimatePresence initial={false}>
-                {isOpen && (
-                    <motion.div
-                        ref={contentRef}
-                        initial={{ height: 0, overflow: 'auto' }}
-                        animate={{ height: 'auto', overflow: 'auto' }}
-                        exit={{ height: 0, overflow: 'auto' }}
-                        transition={{ duration: 0.4, ease: 'easeInOut' }}
-                        onAnimationComplete={handleAnimationComplete}
-                    >
-                        <div className={styles.content}>{content}</div>
-                    </motion.div>
-                )}
-            </AnimatePresence>
         </div>
     );
 };
