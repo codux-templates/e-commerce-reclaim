04ef3bcb0a3a239b537f29f393d944adbf9a3e78
Tetiana Mandziuk <tetianaman@wix.com>
2024-10-08 12:16:50 +0200

    tweak

--- app/routes/cart/route.tsx
+++ app/routes/cart/route.tsx
@@ -23,69 +23,68 @@ export default function CartPage() {
         }
     };
 
+    if (!cart.data?.lineItems.length)
+        return (
+            <div className={styles.cart}>
+                <h1 className={styles.cartHeader}>My cart</h1>
+                <div className={styles.emptyCartMessage}>
+                    <div>Cart is empty</div>
+                    <NavLink to={ROUTES.home.to()} className={styles.continueBrowsingLink}>
+                        Continue Browsing
+                    </NavLink>
+                </div>
+            </div>
+        );
+
     const priceSummary = cartTotals.data?.priceSummary;
     return (
         <div className={styles.page}>
-            {cart.data && cart.data.lineItems.length > 0 ? (
-                <>
-                    <div className={styles.cart}>
-                        <h1 className={styles.cartHeader}>My cart</h1>
-                        <div className={styles.cartItems}>
-                            {cart.data.lineItems.map((item) => (
-                                <CartItem
-                                    key={item._id}
-                                    item={item}
-                                    priceBreakdown={findLineItemPriceBreakdown(item)}
-                                />
-                            ))}
-                        </div>
+            <div className={styles.cart}>
+                <h1 className={styles.cartHeader}>My cart</h1>
+                <div className={styles.cartItems}>
+                    {cart.data.lineItems.map((item) => (
+                        <CartItem
+                            key={item._id}
+                            item={item}
+                            priceBreakdown={findLineItemPriceBreakdown(item)}
+                        />
+                    ))}
+                </div>
+            </div>
+            <div className={styles.summary}>
+                <h1 className={styles.summaryHeader}>Order summary</h1>
+                <div className={styles.summarySection}>
+                    <div className={styles.summaryRow}>
+                        <span>Subtotal</span>
+                        <span>{priceSummary?.subtotal?.formattedConvertedAmount}</span>
                     </div>
-                    <div className={styles.summary}>
-                        <h1 className={styles.summaryHeader}>Order summary</h1>
-                        <div className={styles.summarySection}>
-                            <div className={styles.summaryRow}>
-                                <span>Subtotal</span>
-                                <span>{priceSummary?.subtotal?.formattedConvertedAmount}</span>
-                            </div>
-                            <div className={styles.summaryRow}>
-                                <span>Delivery</span>
-                                <span>{priceSummary?.shipping?.formattedConvertedAmount}</span>
-                            </div>
-                            <div className={styles.summaryRow}>
-                                <span className={styles.location}>Poland</span>
-                            </div>
-                        </div>
-
-                        <div className={styles.summarySection}>
-                            <div className={classNames(styles.summaryRow, styles.summaryTotal)}>
-                                <span>Total</span>
-                                <span>{priceSummary?.total?.formattedConvertedAmount}</span>
-                            </div>
-                            <button
-                                className={classNames('button', styles.checkoutButton)}
-                                onClick={handleCheckout}
-                            >
-                                Checkout
-                            </button>
-
-                            <div className={styles.secureCheckout}>
-                                <LockIcon width={11} />
-                                <span>Secure Checkout</span>
-                            </div>
-                        </div>
+                    <div className={styles.summaryRow}>
+                        <span>Delivery</span>
+                        <span>{priceSummary?.shipping?.formattedConvertedAmount}</span>
                     </div>
-                </>
-            ) : (
-                <div className={styles.cart}>
-                    <h1 className={styles.cartHeader}>My cart</h1>
-                    <div className={styles.emptyCartMessage}>
-                        <div>Cart is empty</div>
-                        <NavLink to={ROUTES.home.to()} className={styles.continueBrowsingLink}>
-                            Continue Browsing
-                        </NavLink>
+                    <div className={styles.summaryRow}>
+                        <span className={styles.location}>Poland</span>
                     </div>
                 </div>
-            )}
+
+                <div className={styles.summarySection}>
+                    <div className={classNames(styles.summaryRow, styles.summaryTotal)}>
+                        <span>Total</span>
+                        <span>{priceSummary?.total?.formattedConvertedAmount}</span>
+                    </div>
+                    <button
+                        className={classNames('button', styles.checkoutButton)}
+                        onClick={handleCheckout}
+                    >
+                        Checkout
+                    </button>
+
+                    <div className={styles.secureCheckout}>
+                        <LockIcon width={11} />
+                        <span>Secure Checkout</span>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
