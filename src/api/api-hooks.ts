5f35d023fa9ecc5ab6d3f46da5e58fe0ee4c98a9
Tetiana Mandziuk <tetianaman@wix.com>
2024-10-15 14:48:58 +0200

    redundant import

--- src/api/api-hooks.ts
+++ src/api/api-hooks.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import useSwr, { Key } from 'swr';
 import useSWRMutation from 'swr/mutation';
 import { findItemIdInCart } from './cart-helpers';
