a1d8a0afe2a72f9089d90756d4295d7e720f2698
Oleh Rak <olehr@wix.com>
2024-11-05 13:41:16 +0200

    support className in props

--- src/components/avatar/avatar.tsx
+++ src/components/avatar/avatar.tsx
@@ -1,14 +1,16 @@
+import classNames from 'classnames';
 import { UserIcon } from '../icons';
 
 import styles from './avatar.module.scss';
 
 export interface AvatarProps {
+    className?: string;
     imageSrc: string | undefined;
 }
 
-export const Avatar = ({ imageSrc }: AvatarProps) => {
+export const Avatar = ({ className, imageSrc }: AvatarProps) => {
     return (
-        <div className={styles.root}>
+        <div className={classNames(styles.root, className)}>
             {imageSrc ? <img src={imageSrc} alt="" /> : <UserIcon className={styles.userIcon} />}
         </div>
     );
