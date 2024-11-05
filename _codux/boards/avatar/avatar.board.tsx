b5052d5ce8b4cfe211c81bdbee2a8165862c9a23
Oleh Rak <olehr@wix.com>
2024-11-05 13:26:24 +0200

    use https url instead of data url

--- _codux/boards/avatar/avatar.board.tsx
+++ _codux/boards/avatar/avatar.board.tsx
@@ -1,6 +1,5 @@
 import { createBoard } from '@wixc3/react-board';
 import { Avatar } from '~/src/components/avatar/avatar';
-import { catImage } from './cat-image';
 
 import styles from './avatar.board.module.scss';
 
@@ -9,9 +8,12 @@ export default createBoard({
     Board: () => {
         return (
             <div className={styles.container}>
-                <Avatar imageSrc={undefined} altText="unknown user avatar" />
-
-                <Avatar imageSrc={catImage} altText="Codux" />
+                <Avatar imageSrc={undefined} />
+                <Avatar
+                    imageSrc={
+                        'https://static.wixstatic.com/media/d8b1a6aa64c642e28d9c2b13665868be.jpg/v1/crop/x_1842,y_741,w_2250,h_2051/fill/w_424,h_388,fp_0.50_0.50,q_80,usm_0.66_1.00_0.01,enc_auto/Maneki-neko.jpg'
+                    }
+                />
             </div>
         );
     },
--- src/components/avatar/avatar.tsx
+++ src/components/avatar/avatar.tsx
@@ -4,17 +4,12 @@ import styles from './avatar.module.scss';
 
 export interface AvatarProps {
     imageSrc: string | undefined;
-    altText: string;
 }
 
-export const Avatar = ({ imageSrc, altText }: AvatarProps) => {
+export const Avatar = ({ imageSrc }: AvatarProps) => {
     return (
         <div className={styles.root}>
-            {imageSrc ? (
-                <img alt={altText} src={imageSrc} />
-            ) : (
-                <UserIcon className={styles.userIcon} />
-            )}
+            {imageSrc ? <img src={imageSrc} alt="" /> : <UserIcon className={styles.userIcon} />}
         </div>
     );
 };
