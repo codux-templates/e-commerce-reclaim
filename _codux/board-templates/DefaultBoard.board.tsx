442e9631ee6b3534f9349809d5432bd48e52db60
Tetiana Mandziuk <tetianaman@wix.com>
2024-11-08 17:32:29 +0100

    feat: add board template with component wrapper

---
+++ _codux/board-templates/DefaultBoard.board.tsx
@@ -0,0 +1,8 @@
+import { createBoard } from '@wixc3/react-board';
+import ComponentWrapper from '_codux/board-wrappers/component-wrapper';
+
+export default createBoard({
+    name: 'New Board',
+    Board: () => <ComponentWrapper />,
+    isSnippet: false,
+});
