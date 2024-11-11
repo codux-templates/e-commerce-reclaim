df4df52d4c2e23c2c2c0ae3d4f90e6fd6d8d9d5e
Oleh Rak <olehr@wix.com>
2024-11-11 16:54:00 +0200

    debug login failure

--- app/routes/login/route.tsx
+++ app/routes/login/route.tsx
@@ -1,23 +1,29 @@
 import { LoaderFunctionArgs, redirect } from '@remix-run/node';
 import { commitSession, getSession, initializeEcomApiForRequest } from '~/src/wix/ecom/session';
+import { getErrorMessage } from '~/src/wix/utils';
 
 export async function loader({ request }: LoaderFunctionArgs) {
-    const currentUrl = new URL(request.url);
-    const returnUrl = request.headers.get('Referer') ?? currentUrl.origin;
+    try {
+        throw new Error('something went wrong');
+        const currentUrl = new URL(request.url);
+        const returnUrl = request.headers.get('Referer') ?? currentUrl.origin;
 
-    const api = await initializeEcomApiForRequest(request);
-    const { oAuthData, authUrl } = await api.login(
-        `${currentUrl.origin}/login-callback`,
-        returnUrl,
-    );
+        const api = await initializeEcomApiForRequest(request);
+        const { oAuthData, authUrl } = await api.login(
+            `${currentUrl.origin}/login-callback`,
+            returnUrl,
+        );
 
-    const session = await getSession(request.headers.get('Cookie'));
+        const session = await getSession(request.headers.get('Cookie'));
 
-    session.flash('oAuthData', oAuthData);
+        session.flash('oAuthData', oAuthData);
 
-    return redirect(authUrl, {
-        headers: {
-            'Set-Cookie': await commitSession(session),
-        },
-    });
+        return redirect(authUrl, {
+            headers: {
+                'Set-Cookie': await commitSession(session),
+            },
+        });
+    } catch (e: unknown) {
+        return getErrorMessage(e);
+    }
 }
