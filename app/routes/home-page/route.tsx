f9838ce8e5612638f225e634bd582ae9c1a2ff91
Codux <codux@codux.com>
2024-12-16 10:36:20 +0200

    dfsdsr

---
+++ app/routes/home-page/route.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+export default function Homepage() {
+    return (
+        <div>
+            <main>
+                <section>
+                    <h1>Shop top brands</h1>
+                </section>
+                <section>
+                    <div>products gallery</div>
+                </section>
+                <section>
+                    <div>new in gallery</div>
+                </section>
+            </main>
+        </div>
+    );
+}
