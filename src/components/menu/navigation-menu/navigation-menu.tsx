c7c7c5140793098fba742854d99b0c8d066d5fbb
matan georgi <matang@wix.com>
2025-01-02 15:22:40 +0200

    feat: add context for closing sidebar menu and manage dialog state

--- src/components/menu/navigation-menu/navigation-menu.tsx
+++ src/components/menu/navigation-menu/navigation-menu.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { createContext, useState } from 'react';
 import * as RadixNavigationMenu from '@radix-ui/react-navigation-menu';
 import {
     Trigger,
@@ -19,42 +19,54 @@ interface NavigationMenuProps
     extends Omit<RadixNavigationMenuProps, 'orientation' | 'aria-orientation'>,
         React.RefAttributes<HTMLDivElement> {}
 
+export const CloseSideBarMenuContext = createContext<() => void>(() => {});
+
 export const NavigationMenu = React.forwardRef<
     React.ElementRef<typeof RadixNavigationMenu.Root>,
     NavigationMenuProps
->(({ className, children, ...props }, forwardedRef) => (
-    <div className={classNames(styles.root, className)}>
-        <nav className={styles.desktopMenu}>
-            <Root {...props} ref={forwardedRef}>
-                <List className={styles.menuList}>{children}</List>
-            </Root>
-        </nav>
-        <nav className={styles.mobileMenu}>
-            <DialogRoot>
-                <Trigger asChild>
-                    <button className={styles.iconButton} aria-label="Open Menu">
-                        <MenuIcon width={24} className={styles.menuIcon} />
-                    </button>
-                </Trigger>
-                <Portal>
-                    <Overlay className={styles.dialogOverlay} />
-                    <Content className={styles.dialogContent}>
-                        <Close asChild>
-                            <button
-                                className={classNames(styles.iconButton, styles.closeButton)}
-                                aria-label="Close"
-                            >
-                                <CrossIcon width={30} className={styles.closeIcon} />
-                            </button>
-                        </Close>
-                        <Root>
-                            <List className={styles.menuList}>{children}</List>
-                        </Root>
-                    </Content>
-                </Portal>
-            </DialogRoot>
-        </nav>
-    </div>
-));
+>(({ className, children, ...props }, forwardedRef) => {
+    const [isDialogOpen, setIsDialogOpen] = useState(false);
+
+    return (
+        <div className={classNames(styles.root, className)}>
+            <nav className={styles.desktopMenu}>
+                <Root {...props} ref={forwardedRef}>
+                    <List className={styles.menuList}>{children}</List>
+                </Root>
+            </nav>
+            <nav className={styles.mobileMenu}>
+                <DialogRoot open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+                    <Trigger asChild>
+                        <button className={styles.iconButton} aria-label="Open Menu">
+                            <MenuIcon width={24} className={styles.menuIcon} />
+                        </button>
+                    </Trigger>
+                    <Portal>
+                        <Overlay className={styles.dialogOverlay} />
+                        <Content className={styles.dialogContent}>
+                            <Close asChild>
+                                <button
+                                    className={classNames(styles.iconButton, styles.closeButton)}
+                                    aria-label="Close"
+                                >
+                                    <CrossIcon width={30} className={styles.closeIcon} />
+                                </button>
+                            </Close>
+                            <Root>
+                                <List className={styles.menuList}>
+                                    <CloseSideBarMenuContext.Provider
+                                        value={() => setIsDialogOpen(false)}
+                                    >
+                                        {children}
+                                    </CloseSideBarMenuContext.Provider>
+                                </List>
+                            </Root>
+                        </Content>
+                    </Portal>
+                </DialogRoot>
+            </nav>
+        </div>
+    );
+});
 
 NavigationMenu.displayName = 'NavigationMenu';
